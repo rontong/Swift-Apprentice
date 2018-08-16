5f2f400bf6811febf982879bbad67c9bddd9cd19
Ron Tong <ronald_tong@hotmail.com>
2018-08-17 07:54:36 +1000

    Chapter 16 - Enumerations

--- Chapter 16 - Enumerations.playground/Contents.swift
+++ Chapter 16 - Enumerations.playground/Contents.swift
@@ -20,7 +20,7 @@ enum Month: Int {
     }
     
     var monthsUntilWinterBreak : Int {
-        return Month.december.rawValue - month.rawValue
+        return Month.december.rawValue - self.rawValue
     }
 }
 
@@ -46,23 +46,6 @@ func monthsUntilWinterBreak(from month: Month) -> Int {
 let fifthMonth = Month(rawValue: 5)
 let monthsLeft = fifthMonth?.monthsUntilWinterBreak
 
-enum Coin: Int {
-    case penny = 1
-    case nickel = 5
-    case dime = 10
-    case quarter = 25
-}
-
-let coin = Coin.quarter
-coin.rawValue
-
-var coinPurse = [Coin]()
-coinPurse.append(Coin.dime)
-coinPurse.append(Coin.nickel)
-coinPurse.append(Coin.quarter)
-coinPurse.append(Coin.quarter)
-coinPurse.append(Coin.penny)
-
 var balance = 100
 
 enum WithdrawalResult {
@@ -87,3 +70,113 @@ case .success(let newBalance):
 case .error(let message):
     print(message)
 }
+
+enum Light {
+    case on
+    case off
+}
+
+let light = Light.off
+light
+
+// Challenge A
+print("*** Challenge A")
+
+enum Coin: Int {
+    case penny = 1
+    case nickel = 5
+    case dime = 10
+    case quarter = 25
+}
+
+let coin = Coin.quarter
+coin.rawValue
+
+var coinPurse : [Coin] = [.penny, .quarter, .nickel, .dime, .penny, .dime, .quarter]
+
+func countCoins(purse: [Coin]) -> Int {
+    
+    let cents = purse.reduce(0) {
+        $0 + $1.rawValue
+    }
+    return cents
+}
+
+countCoins(purse: coinPurse)
+
+// Challenge B
+print("*** Challenge B")
+
+enum Month2: Int {
+    case january = 1, february, march, april, may, june, july, august, september, october, november, december
+    
+    var semester : String {
+        switch self {
+        case .august, . september, .october, .november, .december:
+            return "Autumn"
+        case .january, .february, .march, .april, .may:
+            return "Spring"
+        case .june, .july:
+            return "Summer"
+        default:
+            return "Not in School Semester"
+        }
+    }
+    
+    var monthsUntilWinterBreak : Int {
+        return Month.december.rawValue - self.rawValue
+    }
+    
+    var monthsUntilSummerBreak : Int {
+        if Month2.june.rawValue > self.rawValue {
+            return Month2.june.rawValue - self.rawValue
+        } else {
+            return self.monthsUntilWinterBreak + Month2.june.rawValue
+        }
+    }
+}
+
+let january = Month2.january
+january.monthsUntilSummerBreak
+
+let september = Month2.september
+september.monthsUntilWinterBreak
+september.monthsUntilSummerBreak
+
+// Challenge C
+print("*** Challenge C")
+
+var xLocation = 0
+var yLocation = 0
+
+enum Direction {
+    case north
+    case south
+    case east
+    case west
+    
+    func coordinates(for direction: Direction) {
+        switch self {
+        case .north: yLocation += 1
+        case .south: yLocation -= 1
+        case .east: xLocation += 1
+        case .west: xLocation -= 1
+        }
+    }
+}
+
+let movements : [Direction] = [.north, .north, .west, .south, .west, .south, .south, .east, .east, .south, .east]
+
+func calculateLocation(movements: [Direction]) -> (x: Int, y: Int) {
+  
+    for movement in movements {
+        movement.coordinates(for: movement)
+        print("Moving \(movement), x is now \(xLocation), y is now \(yLocation)")
+    }
+    
+    let location = (x: xLocation, y: yLocation)
+
+    return location
+}
+
+calculateLocation(movements: movements)
